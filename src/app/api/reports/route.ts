a03620035755bc0685e1787401d5e7b47b055aa9
Harikumar Patel <harikumarpatel@Harikumars-MacBook-Pro.local>
2025-12-07 22:14:36 -0800

    Add revalidate=0 to API route to fix static gen error

--- src/app/api/reports/route.ts
+++ src/app/api/reports/route.ts
@@ -5,6 +5,7 @@ import prisma from "@/lib/prisma";
 import { startOfWeek, startOfMonth } from "date-fns";
 
 export const dynamic = "force-dynamic";
+export const revalidate = 0;
 
 export async function GET(request: Request) {
     try {
